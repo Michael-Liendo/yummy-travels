6d0fd3b835ba40f3344100908da2dd76faa5c236
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-26 15:53:36 -0400

    feat: find the travel (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./routes/Home.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home.tsx';
+import HomePage from './routes/HomePage.tsx';
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path="/home" element={<HomePage />} />
     </Routes>
   </BrowserRouter>
 );
