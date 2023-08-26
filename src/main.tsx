c9697fe670d142c5daa93f4ec47cd469db1f78f1
Javier A Mora gil <j4viermora@gmail.com>
2023-08-26 17:20:18 -0400

    some change

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './routes/Home.tsx';
-import HomePage from './routes/HomePage.tsx';
-import Booking from './routes/Booking.tsx';
-
-
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./routes/HomePage.tsx";
+import Booking from "./routes/Booking.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/booking" element={<Booking />} />
     </Routes>
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,94 +1,94 @@
-import { Label, TextInput, Button } from 'flowbite-react';
-import { FormEvent, useState } from 'react';
-
+import { Label, TextInput, Button } from "flowbite-react";
+import { FormEvent, useState } from "react";
 
 export default function App() {
-  const [travelForm, setTravelForm] = useState<{ current_address: string, address: string, travel_date: string, passengers: number }>({ current_address: '', address: '', travel_date: '', passengers: 0 })
+  const [travelForm, setTravelForm] = useState<{
+    current_address: string;
+    address: string;
+    travel_date: string;
+    passengers: number;
+  }>({ current_address: "", address: "", travel_date: "", passengers: 0 });
 
   async function handleSubmit(e: FormEvent) {
-    e.preventDefault()
+    e.preventDefault();
 
-    console.log(travelForm)
+    console.log(travelForm);
   }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTravelForm({ ...travelForm, [e.target.name]: e.target.value })
-  }
+    setTravelForm({ ...travelForm, [e.target.name]: e.target.value });
+  };
   return (
     <form onSubmit={handleSubmit} className="px-5 mt-5">
       <h1 className="text-2xl">¡Viaja ahora!</h1>
 
-      <div className='my-5'>
+      <div className="my-5">
         <div className="mb-2 block">
-          <Label
-            htmlFor="current-address"
-            value="Ubicación"
-          />
+          <Label htmlFor="current-address" value="Ubicación" />
         </div>
         <TextInput
-          id='current-address'
-          name='current_address'
+          id="current-address"
+          name="current_address"
           placeholder="Valencia, Carabobo"
           required
-          onChange={(e) => { handleInputChange(e) }}
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
           type="text"
         />
       </div>
-      <div className='my-5'>
+      <div className="my-5">
         <div className="mb-2 block">
-          <Label
-            htmlFor="address"
-            value="¿A donde quieres ir?"
-          />
+          <Label htmlFor="address" value="¿A donde quieres ir?" />
         </div>
         <TextInput
-          name='address'
-          id='address'
+          name="address"
+          id="address"
           placeholder="Caracas"
           required
-          onChange={(e) => { handleInputChange(e) }}
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
           type="text"
         />
       </div>
-      <div className='flex justify-between space-x-4 my-5'>
+      <div className="flex justify-between space-x-4 my-5">
         <div>
-
           <div className="mb-2 block">
-            <Label
-              htmlFor="date"
-              value="Fecha de viaje"
-            />
+            <Label htmlFor="date" value="Fecha de viaje" />
           </div>
           <TextInput
-            name='travel_date'
-            id='date'
+            name="travel_date"
+            id="date"
             placeholder="20/09/2023"
             required
-            onChange={(e) => { handleInputChange(e) }}
+            onChange={(e) => {
+              handleInputChange(e);
+            }}
             type="date"
           />
         </div>
         <div>
-
           <div className="mb-2 block">
-            <Label
-              htmlFor="passengers"
-              value="Pasajeros"
-            />
+            <Label htmlFor="passengers" value="Pasajeros" />
           </div>
           <TextInput
-            name='passengers'
-            id='passengers'
+            name="passengers"
+            id="passengers"
             placeholder="1"
             required
-            onChange={(e) => { handleInputChange(e) }}
+            onChange={(e) => {
+              handleInputChange(e);
+            }}
             type="number"
           />
         </div>
       </div>
-      <div className='flex justify-end'>
-        <Button color="purple" type="submit">Buscar viaje</Button>
+      <div className="flex justify-end">
+        <Button color="purple" type="submit">
+          Buscar viaje
+        </Button>
       </div>
     </form>
-  )
+  );
 }
