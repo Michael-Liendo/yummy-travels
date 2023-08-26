685746e8736b3e88a9065d360c4df3eae053e764
Sergionx <sergio180203@gmail.com>
2023-08-26 16:16:34 -0400

    added colors

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -5,8 +5,26 @@ import TravelCard from "./TravelCard";
 
 export default function TabsWithIcons() {
   return (
-    <Tabs.Group aria-label="Tabs with icons" style="underline" >
-      <Tabs.Item active icon={BsBusFrontFill} title="Autobus" >
+    <Tabs.Group
+      aria-label="Travel modes"
+      style="underline"
+      theme={{
+        tablist: {
+          tabitem: {
+            base: "flex items-center justify-center p-4 rounded-t-lg text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500 focus:ring-4 focus:ring-primary focus:outline-none",
+            styles: {
+              underline: {
+                active: {
+                  on: "text-primary dark:text-primary border-b-2 border-primary dark:border-primary",
+                  off: "border-b-2 border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300",
+                },
+              },
+            },
+          },
+        },
+      }}
+    >
+      <Tabs.Item active icon={BsBusFrontFill} title="Autobus">
         <TravelCard />
       </Tabs.Item>
       <Tabs.Item icon={BsAirplaneFill} title="Avion">
