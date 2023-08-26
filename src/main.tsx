ce0a78adddecca7d1b76de64bc70e92936e2182b
Javier A Mora gil <j4viermora@gmail.com>
2023-08-26 17:10:20 -0400

    style: travel detail

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home.tsx";
+import { TravelDetails } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/details" element={<TravelDetails />} />
     </Routes>
   </BrowserRouter>
 );
