49cf058fd0f50e8e5d5da9a57ba84ab6c9fa082f
Sergionx <sergio180203@gmail.com>
2023-08-26 19:40:16 -0400

    trip

---
+++ src/models/Trip.ts
@@ -0,0 +1,12 @@
+interface Trip {
+  id: number;
+  type: string;
+  price: number;
+  description: string;
+  city: string;
+  destination: string;
+  date: string;
+  time: string;
+  arrivalDate: string;
+  arrivalHour: string;
+}
