998afad65300977ddded4e783d1f47352b8ac3c5
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-26 19:16:04 -0400

    feat: checkout (#16)

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TravelDetails } from "./routes";
-import "./index.css";
 import HomePage from "./routes/HomePage.tsx";
 import Booking from "./routes/Booking.tsx";
 import PassengerDetail from "./routes/PassengerDetail.tsx";
+import ReactDOM from 'react-dom/client'
+import Home from './routes/Home.tsx';
+import Checkout from './routes/Checkout.tsx';
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path='/' element={<Home />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/booking" element={<Booking />} />
       <Route path="/booking/details" element={<TravelDetails />} />
       {/* FIXME - Meterle el id al viaje */}
       <Route path="/passenger-detail" element={<PassengerDetail />} />
+      <Route path='/checkout' element={<Checkout />} />
     </Routes>
   </BrowserRouter>
 );
