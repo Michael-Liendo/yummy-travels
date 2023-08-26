de7c8c67ce03f3b33aaeb9588e72fa0f180106e9
Javier A Mora gil <j4viermora@gmail.com>
2023-08-26 17:27:31 -0400

    style: simple style changes, add app layout

--- src/routes/Booking.tsx
+++ src/routes/Booking.tsx
@@ -2,53 +2,56 @@ import { Tabs } from "flowbite-react";
 import { AiFillCar } from "react-icons/ai";
 import { BsAirplaneFill, BsBusFrontFill } from "react-icons/bs";
 import TravelCard from "../components/TravelCard";
+import { AppLayout } from "../layout";
 
 export default function Booking() {
   return (
-    <Tabs.Group
-      aria-label="Travel modes"
-      style="underline"
-      theme={{
-        tablist: {
-          tabitem: {
-            base: "flex items-center justify-center p-4 rounded-t-lg text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500 focus:ring-[3px] focus:ring-primary focus:outline-none",
-            styles: {
-              underline: {
-                active: {
-                  on: "text-primary dark:text-primary border-b-2 border-primary dark:border-primary",
-                  off: "border-b-2 border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300",
+    <AppLayout>
+      <Tabs.Group
+        aria-label="Travel modes"
+        style="underline"
+        theme={{
+          tablist: {
+            tabitem: {
+              base: "flex items-center justify-center p-4 rounded-t-lg text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500 focus:ring-[3px] focus:ring-primary focus:outline-none",
+              styles: {
+                underline: {
+                  active: {
+                    on: "text-primary dark:text-primary border-b-2 border-primary dark:border-primary",
+                    off: "border-b-2 border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300",
+                  },
                 },
               },
             },
           },
-        },
-      }}
-    >
-      <Tabs.Item active icon={BsBusFrontFill} title="Autobus">
-        <TravelCard />
-      </Tabs.Item>
-      <Tabs.Item icon={BsAirplaneFill} title="Avion">
-        <p>
-          This is
-          <span className="font-medium text-gray-800 dark:text-white">
-            Dashboard tab's associated content
-          </span>
-          . Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </p>
-      </Tabs.Item>
-      <Tabs.Item icon={AiFillCar} title="Carro por puesto">
-        <p>
-          This is
-          <span className="font-medium text-gray-800 dark:text-white">
-            Settings tab's associated content
-          </span>
-          . Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </p>
-      </Tabs.Item>
-    </Tabs.Group>
+        }}
+      >
+        <Tabs.Item active icon={BsBusFrontFill} title="Autobus">
+          <TravelCard />
+        </Tabs.Item>
+        <Tabs.Item icon={BsAirplaneFill} title="Avion">
+          <p>
+            This is
+            <span className="font-medium text-gray-800 dark:text-white">
+              Dashboard tab's associated content
+            </span>
+            . Clicking another tab will toggle the visibility of this one for
+            the next. The tab JavaScript swaps classes to control the content
+            visibility and styling.
+          </p>
+        </Tabs.Item>
+        <Tabs.Item icon={AiFillCar} title="Carro">
+          <p>
+            This is
+            <span className="font-medium text-gray-800 dark:text-white">
+              Settings tab's associated content
+            </span>
+            . Clicking another tab will toggle the visibility of this one for
+            the next. The tab JavaScript swaps classes to control the content
+            visibility and styling.
+          </p>
+        </Tabs.Item>
+      </Tabs.Group>
+    </AppLayout>
   );
 }
