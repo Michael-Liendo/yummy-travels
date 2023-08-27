785d0ba92f77ecf06351e8b30582ee7ca43bd84f
David Arenas <davejs136@gmail.com>
2023-08-27 01:05:09 -0400

    feat: add `Inter` fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,20 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif", "system-ui"],
+      },
       colors: {
-        primary: '#6c2bd9',
-        "primary-strong": '#6B5FCB',
+        primary: "#6c2bd9",
+        "primary-strong": "#6B5FCB",
         secondary: "#06A77D",
-        tertiary: '#F9FAFB',
-      }
+        tertiary: "#F9FAFB",
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
