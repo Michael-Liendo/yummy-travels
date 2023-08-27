2f829584fabba0c5387aeb4873815776fca227b6
David Arenas <davejs136@gmail.com>
2023-08-26 21:43:30 -0400

    refactor: remove unused empty `Home` component

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import HomePage from "./routes/HomePage.tsx";
 import Booking from "./routes/Booking.tsx";
 import PassengerDetail from "./routes/PassengerDetail.tsx";
 import ReactDOM from 'react-dom/client'
-import Home from './routes/Home.tsx';
 import Checkout from './routes/Checkout.tsx';
 
 
@@ -13,8 +12,7 @@ import Checkout from './routes/Checkout.tsx';
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path="/home" element={<HomePage />} />
+      <Route path='/' element={<HomePage />} />
       <Route path="/booking" element={<Booking />} />
       <Route path="/booking/details" element={<TravelDetails />} />
       {/* FIXME - Meterle el id al viaje */}
