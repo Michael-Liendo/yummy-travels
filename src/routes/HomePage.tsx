8ddfb3e1f1cf4b08a487805ec3f229f923847fdd
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-26 22:27:48 -0400

    style: button bottom margin

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -118,7 +118,7 @@ export default function App() {
               },
             }}
             color="primary"
-            className="w-full"
+            className="w-full mb-5"
           >
             Buscar viaje
           </Button>
