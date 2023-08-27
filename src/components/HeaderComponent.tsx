80f09c801b685627c28f59b9bb3f37426d99eb6c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-26 23:31:50 -0400

    feat: icons

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,7 +1,10 @@
 import { Button } from "flowbite-react";
 import { AiOutlineLeft } from "react-icons/ai";
+import { BiCurrentLocation } from "react-icons/bi";
+import { FaLocationArrow } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useApp } from "../store/app";
+import { MdOutlineDateRange } from "react-icons/md";
 
 export const HeaderComponent = () => {
   const navigate = useNavigate();
@@ -17,18 +20,28 @@ export const HeaderComponent = () => {
           </Button>
         </span>
         <div>
-          <h2 className="text-white">{searchData.current_address}</h2>
-          <p className="text-white text-xs">
-            {searchData.address}, {searchData.passengers}{" "}
-            {searchData.passengers > 1 ? "pasajeros" : "pasajero"}
-          </p>
-          <time className="text-white">
-            {new Date(searchData.travel_date).toLocaleString("es", {
-              month: "long",
-              year: "2-digit",
-              day: "2-digit",
-            })}
-          </time>
+          <div className="flex items-center space-x-0.5">
+
+            <FaLocationArrow color="white" className="mr-2" />
+            <h1 className="text-white text-lg">
+              {searchData.address}, {searchData.passengers}{" "}
+              {searchData.passengers > 1 ? "pasajeros" : "pasajero"}
+            </h1>
+          </div>
+          <div className="flex items-center space-x-0.5">
+            <BiCurrentLocation color="white" className="mr-2" />
+            <h2 className="text-white">{searchData.current_address}</h2>
+          </div>
+          <div className="flex items-center space-x-0.5">
+            <MdOutlineDateRange className="text-white mr-2" />
+            <time className="text-white">
+              {new Date(searchData.travel_date).toLocaleString("es", {
+                month: "long",
+                year: "2-digit",
+                day: "2-digit",
+              })}
+            </time>
+          </div>
         </div>
         <div></div>
       </div>
