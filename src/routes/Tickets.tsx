2221fc846ebbe90857cbd22ac97fda40f1e6ebc9
David Arenas <davejs136@gmail.com>
2023-08-27 11:41:24 -0400

    feat: implement backend data to tickets page

--- src/routes/Tickets.tsx
+++ src/routes/Tickets.tsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 import { Tabs, Card } from "flowbite-react";
 import { MdCardTravel, MdArchive, MdAirplaneTicket } from "react-icons/md";
 import { FaArrowRight } from "react-icons/fa";
-import { tickets } from "../data/tickets";
+// import { tickets } from "../data/tickets";
 import { formatDate, icons } from "../utils";
 import { AppLayout } from "../layout";
-import electionImage from '../assets/election_day.svg'
+import electionImage from "../assets/election_day.svg";
+import { useTickets } from "../hooks/useTickets";
+import { useEffect, useState } from "react";
 
 const NoTickets = () => (
   <section>
@@ -14,6 +16,29 @@ const NoTickets = () => (
     </div>
     <h2>No tienes viajes próximos</h2>
   </section>
+);
+
+const SkeletorLoading = () => (
+  <div className="animate-pulse bg-white mx-4 rounded-md shadow-md mb-4">
+    <div className="w-full h-32 p-5 flex flex-col gap-8">
+      <div className="flex justify-between">
+        <div className="w-28 bg-gray-200 h-3 rounded-md"></div>
+        <div className="w-16 bg-gray-200 h-3 rounded-md"></div>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-2">
+          <div className="w-40 bg-gray-200 h-3 rounded-md"></div>
+          <div className="flex justify-between">
+            <div className="flex gap-2">
+              <div className="w-10 bg-gray-200 h-3 rounded-md"></div>
+              <div className="w-20 bg-gray-200 h-3 rounded-md"></div>
+            </div>
+          </div>
+        </div>
+        <div className="w-10 h-10 bg-gray-200 rounded-md"></div>
+      </div>
+    </div>
+  </div>
 );
 
 const NoArchived = () => (
@@ -25,27 +50,29 @@ const NoArchived = () => (
   </section>
 );
 
-const TicketsContent = () => (
+const TicketsContent = (tickets) => (
   <section className="px-3 flex flex-col gap-4">
-    {tickets.map((ticket) => (
-      <Link to={`/tickets/${ticket.id}`} key={ticket.id}>
+    {tickets.data.map((ticket) => (
+      <Link to={`/tickets/${ticket?.trip?._id}`} key={ticket.id}>
         <Card className="flex flex-col">
           <div className="flex justify-between text-sm text-black/70">
-            <span>{formatDate(ticket.date, { date: true })}</span>
-            <span>{formatDate(ticket.date, { time: true })}</span>
+            <span>{formatDate(ticket?.trip.date, { date: true })}</span>
+            <span>{formatDate(ticket?.trip.date, { time: true })}</span>
           </div>
           <div className="flex justify-between">
             <div className="flex flex-col">
-              <span>{ticket.from}</span>
+              <span className="first-letter:uppercase">
+                {ticket?.trip?.city}
+              </span>
               <span className="flex items-center gap-2">
                 <div className="text-primary-strong">
                   <FaArrowRight />
                 </div>{" "}
-                {ticket.destination}
+                {ticket?.trip?.destination}
               </span>
             </div>
             <div className="text-3xl self-end text-black/60">
-              {icons[ticket.type]}
+              {icons[ticket?.trip.type]}
             </div>
           </div>
         </Card>
@@ -55,6 +82,20 @@ const TicketsContent = () => (
 );
 
 export default function Tickets() {
+  const { data, getAll } = useTickets();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    try {
+      setLoading(true);
+      getAll();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   return (
     <AppLayout>
       <div className="w-[300px] m-auto mb-8">
@@ -63,7 +104,9 @@ export default function Tickets() {
         </div>
       </div>
 
-      <h4 className="text-2xl text-center font-bold text-primary mb-10">Tus boletos</h4>
+      <h4 className="text-2xl text-center font-bold text-primary mb-10">
+        Tus boletos
+      </h4>
       <Tabs.Group
         aria-label="Tabs with underline"
         style="underline"
@@ -84,7 +127,13 @@ export default function Tickets() {
         }}
       >
         <Tabs.Item active icon={MdCardTravel} title="Próximo">
-          {tickets.length ? <TicketsContent /> : <NoTickets />}
+          {loading && !data.length ? (
+            <SkeletorLoading />
+          ) : !loading && data.length ? (
+            <TicketsContent data={data} />
+          ) : (
+            <NoTickets />
+          )}
         </Tabs.Item>
         <Tabs.Item icon={MdArchive} title="Archivados">
           <NoArchived />
